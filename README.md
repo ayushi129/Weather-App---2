da4cba1f1dc3fd871a695cc43e9fc61464bdc992
ayushi129 <54887403+ayushi129@users.noreply.github.com>
2024-03-15 04:46:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather-App-2
+ Another Web App with seven day forecast
---
+++ script.js
@@ -0,0 +1,5 @@
+const time = document.getElementById('time');
+const date = document.getElementById('date');
+const currentWeatherItems = document.getElementById('current-weather-items');
+const timezone = document.getElementById('timezone');
+const country = document.getElementById('country');
